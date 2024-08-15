425c2d1ef181667bcb4cd64141b5b38906d34765
JZmadequan <jzmadequan@tinman.cn>
2024-08-15 10:07:26 +0800

    feat: add sping injection mapstruct

--- ddd-core/src/main/java/cc/mikaka/ddd/core/convertor/UserCoreConvert.java
+++ ddd-core/src/main/java/cc/mikaka/ddd/core/convertor/UserCoreConvert.java
@@ -3,15 +3,19 @@ package cc.mikaka.ddd.core.convertor;
 import cc.mikaka.ddd.core.convertor.mapstruct.UserCoreMapsConvert;
 import cc.mikaka.ddd.core.model.UserModel;
 import cc.mikaka.ddd.dao.UserDO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserCoreConvert {
+    @Autowired
+    UserCoreMapsConvert userCoreMapsConvert;
+
     public UserModel do2Model(UserDO userDO) {
-        return UserCoreMapsConvert.INSTANCE.do2Model(userDO);
+        return userCoreMapsConvert.do2Model(userDO);
     }
 
     public UserDO model2Do(UserModel userModel) {
-        return UserCoreMapsConvert.INSTANCE.model2Do(userModel);
+        return userCoreMapsConvert.model2Do(userModel);
     }
 }
--- ddd-core/src/main/java/cc/mikaka/ddd/core/convertor/mapstruct/UserCoreMapsConvert.java
+++ ddd-core/src/main/java/cc/mikaka/ddd/core/convertor/mapstruct/UserCoreMapsConvert.java
@@ -1,16 +1,16 @@
 package cc.mikaka.ddd.core.convertor.mapstruct;
 
+import cc.mikaka.ddd.common.convert.BaseModelConvert;
 import cc.mikaka.ddd.core.model.UserModel;
 import cc.mikaka.ddd.dao.UserDO;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
 import org.mapstruct.factory.Mappers;
 
 
-@Mapper
-public interface UserCoreMapsConvert {
-    UserCoreMapsConvert INSTANCE = Mappers.getMapper(UserCoreMapsConvert.class);
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserCoreMapsConvert extends BaseModelConvert<UserModel, UserDO> {
 
-    UserModel do2Model(UserDO source);
-
-    UserDO model2Do(UserModel source);
 }
